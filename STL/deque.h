a070ddd9c82c17aba5c6e31eef4b8948fe62e69a
IceCream-jy <304754843@qq.com>
2020-11-12 23:12:46 +0800

    add some functions

--- STL/deque.h
+++ STL/deque.h
@@ -200,42 +200,67 @@ namespace pocket_stl{
 
     public:
         /*************** Iterators *****************/
-        iterator begin() noexcept { return __start().cur; }
-        const_iterator begin() const noexcept { return __start().cur; }
-        iterator end() noexcept { return __finish().cur; }
-        const_iterator end() const noexcept { return __finish().cur; }
-        reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
-        const_reverse_iterator rbegin() const noexcept { return reverse_iterator(end()); }
-        reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
-        const_reverse_iterator rend() const noexcept { return reverse_iterator(begin()); }
-        const_iterator cbegin() const noexcept { return begin(); }
-        const_iterator cend() const noexcept { return end(); }
-        const_reverse_iterator crbegin() const noexcept { return reverse_iterator(end()); }
-        const_reverse_iterator crend() const noexcept { return reverse_iterator(begin()); }
+        iterator                begin() noexcept { return __start().cur; }
+        const_iterator          begin() const noexcept { return __start().cur; }
+        iterator                end() noexcept { return __finish().cur; }
+        const_iterator          end() const noexcept { return __finish().cur; }
+        reverse_iterator        rbegin() noexcept { return reverse_iterator(end()); }
+        const_reverse_iterator  rbegin() const noexcept { return reverse_iterator(end()); }
+        reverse_iterator        rend() noexcept { return reverse_iterator(begin()); }
+        const_reverse_iterator  rend() const noexcept { return reverse_iterator(begin()); }
+        const_iterator          cbegin() const noexcept { return begin(); }
+        const_iterator          cend() const noexcept { return end(); }
+        const_reverse_iterator  crbegin() const noexcept { return reverse_iterator(end()); }
+        const_reverse_iterator  crend() const noexcept { return reverse_iterator(begin()); }
         /*************** Capacity *****************/
         size_type size() const noexcept { return __finish() - __start(); }
         size_type max_size() const noexcept { return static_cast<size_type>(-1); }
 
+        /*************** Element access *****************/
+        reference       front() { return *__start(); }
+        const_reference front() const { return *__start(); }
+        reference       back() { return *(__finish() - 1); }
+        const_reference back() const { return *(__finish() - 1); }
         /*************** Modifiers *****************/
-        iterator insert (const_iterator position, const value_type& val);
-        iterator insert (const_iterator position, size_type n, const value_type& val);
+        void        push_front (const value_type& val);
+        void        push_front(value_type&& val) { emplace_front(std::move(val)); }
+        void        push_back (const value_type& val);
+        void        push_back(value_type&& val) { emplace_back(std::move(val)); }
+        iterator    insert (const_iterator position, const value_type& val);
+        iterator    insert (const_iterator position, size_type n, const value_type& val);
         template <class InputIterator, class std::enable_if<
                     std::is_integral<InputIterator>::value
                     >::type>
-        iterator insert (const_iterator position, InputIterator first, InputIterator last);
-        iterator insert (const_iterator position, value_type&& val);
-        iterator insert (const_iterator position, std::initializer_list<value_type> il);
+        iterator    insert (const_iterator position, InputIterator first, InputIterator last);
+        iterator    insert (const_iterator position, value_type&& val);
+        iterator    insert (const_iterator position, std::initializer_list<value_type> il);
 
-        iterator erase (const_iterator position );
-        iterator erase (const_iterator first, const_iterator last );
+        iterator    erase (const_iterator position );
+        iterator    erase (const_iterator first, const_iterator last );
+        template <class... Args>
+        iterator    emplace (const_iterator position, Args&&... args);
+        template <class... Args>
+        void        emplace_front (Args&&... args);
+        template <class... Args>
+        void        emplace_back (Args&&... args);
     private:
         /***********************辅助工具*****************************/
-        static size_type buffer_size() { return iterator::buffer_size(); }
-        void allocate_and_fill(size_type n, const value_type& val);
+        static size_type    buffer_size() { return iterator::buffer_size(); }
+        void                allocate_and_fill(size_type n, const value_type& val);
         template <class InputItearator>
-        void allocate_and_copy(InputItearator first, InputItearator last);
-        void create_map_and_nodes(size_type num_elementes);
-        void destroy_and_deallocate_all();
+        void                allocate_and_copy(InputItearator first, InputItearator last);
+        pointer             allocate_node() { return __data_allocator().allocate(buffer_size()); }
+        void                create_map_and_nodes(size_type num_elementes);
+        void                destroy_and_deallocate_all();
+        void                expand_at_back(size_type nodes_to_add = 1);
+        void                expand_at_front(size_type nodes_to_add = 1);
+        void                reserve_map_at_back(size_type nodes_to_add = 1);
+        void                reserve_map_at_front(size_type nodes_to_add = 1);
+        void                reallocate_map(size_type nodes_to_add, bool add_at_front);
+        template <class... Args>
+        iterator            insert_aux(iterator pos, Args&&... args);
+        iterator            insert_fill(iterator pos, size_type n, const value_type& val);
+
     };
 
     /*-------------------------------部分函数定义------------------------------------*/
@@ -245,6 +270,131 @@ namespace pocket_stl{
     deque<T, Alloc>::operator=(const deque& x){
         if(this != &x){
 
+        }
+    }
+
+
+    // -------------------- Modifiers
+    template <class T, class Alloc>
+    template <class... Args>
+    typename deque<T, Alloc>::iterator
+    deque<T, Alloc>::emplace(const_iterator position, Args&&... args){
+        if(__start().cur == position.cur){
+            emplace_front(std::forward<Args>(args)...);
+            return __start();
+        }
+        else if(__finish().cur == position.cur){
+            emplace_back(std::forward<Args>(args)...);
+            return __finish() - 1;
+        }
+        else{
+            insert_aux(position, std::forward<Args>(args)...);
+        }
+    }
+
+
+    template <class T, class Alloc>
+    void
+    deque<T, Alloc>::push_back(const value_type& val){
+        if(__finish().cur != __finish().last - 1){
+            __data_allocator().construct(__finish().cur, val);
+            ++__finish().cur;
+        }
+        else{
+            expand_at_back();
+            __data_allocator().construct(__finish().cur, val);
+            ++__finish();
+        }
+    }
+
+    template <class T, class Alloc>
+    void
+    deque<T, Alloc>::push_front(const value_type& val){
+        if(__finish().cur != __start().first){
+            __data_allocator().construct(__start().cur - 1, val);
+            --__start().cur;
+        }
+        else{
+            expand_at_front();
+            __data_allocator().construct(__start().cur - 1, val);
+            --__start();
+        }
+    }
+
+    template <class T, class Alloc>
+    typename deque<T, Alloc>::iterator
+    deque<T, Alloc>::insert(const_iterator position, const value_type& val){
+        if(__start().cur == position.cur){
+            push_front(val);
+            return __start();
+        }
+        else if(__finish().cur == position.cur){
+            push_back(val);
+            return __finish() - 1;
+        }
+        else{
+            insert_aux(position, val);
+        }
+    }
+
+    template <class T, class Alloc>
+    typename deque<T, Alloc>::iterator
+    deque<T, Alloc>::insert(const_iterator position, value_type&& val){
+        return emplace(position, std::move(val));
+    }
+
+    template <class T, class Alloc>
+    typename deque<T, Alloc>::iterator
+    deque<T, Alloc>::insert(const_iterator position, size_type n, const value_type& val){
+        if(position.cur == __start().cur){
+            int nodes_to_add = (n - __start().cur + __start().first) / buffer_size();
+            expand_at_front(nodes_to_add);
+            iterator new_begin = __start() - n;
+            uninitialized_fill_n(new_begin, n, val);
+            __start() = new_begin;
+            return __start();
+        }
+        else if(position.cur == __finish().cur){
+            int nodes_to_add = (n - __finish().last + __start().cur + 1) / buffer_size();
+            expand_at_back(nodes_to_add);
+            iterator new_end = __finish() + n;
+            iterator old_end = __finish();
+            uninitialized_fill_n(__finish(), n, val);
+            __finish() = new_end;
+            return old_end;
+        }
+        else{
+            return insert_fill(position, n, val);
+        }
+    }
+
+    template <class T, class Alloc>
+    template <class... Args>
+    void
+    deque<T, Alloc>::emplace_back(Args&&... args){
+        if(__finish().cur != __finish().last - 1){
+            __data_allocator().construct(__finish().cur, std::forward<Args>(args)...);
+            ++__finish().cur;
+        }
+        else{
+            expand_at_back();
+            __data_allocator().construct(__finish().cur, std::forward<Args>(args)...);
+            ++__finish();
+        }
+    }
+
+    template <class T, class Alloc>
+    template <class... Args>
+    void
+    deque<T, Alloc>::emplace_front(Args&&... args){
+        if(__finish().cur != __start().first){
+            __data_allocator().construct(__start().cur - 1, std::forward<Args>(args)...);
+            --__start().cur;
+        }
+        else{
+            expand_at_front();
+            __data_allocator().construct(__start().cur - 1, std::forward<Args>(args)...);
+            --__start();
         }
     }
 
@@ -289,7 +439,7 @@ namespace pocket_stl{
         map_pointer cur;
         try{
             for (cur = nstart; cur <= nfinish; ++cur){
-                *cur = __data_allocator().allocate(buffer_size());
+                *cur = allocate_node();
             }
         }
         catch(...){
@@ -317,6 +467,98 @@ namespace pocket_stl{
         }
         __data_allocator().deallocate(*__start().node, buffer_size());
         __data_allocator().deallocate(__map, __map_size);
+    }
+
+    template <class T, class Alloc>
+    void
+    deque<T, Alloc>::expand_at_back(size_type nodes_to_add){
+        reserve_map_at_back(nodes_to_add);
+        for (int i = 1; i <= nodes_to_add; ++i){
+            *(__finish().node + i) = allocate_node();
+        }
+    }
+
+    template <class T, class Alloc>
+    void
+    deque<T, Alloc>::expand_at_front(size_type nodes_to_add){
+        reserve_map_at_front(nodes_to_add);
+        for (int i = 1; i <= nodes_to_add; ++i){
+            *(__start().node - i) = allocate_node();
+        }
+    }
+
+    template <class T, class Alloc>
+    void
+    deque<T, Alloc>::reserve_map_at_back(size_type nodes_to_add){
+        if(nodes_to_add + 1 > __map_size  - (__finish().node - __map)){
+            reallocate_map(nodes_to_add, false);
+        }
+    }
+
+    template <class T, class Alloc>
+    void
+    deque<T, Alloc>::reserve_map_at_front(size_type nodes_to_add){
+        if(nodes_to_add + 1 > __start().node - __map){
+            reallocate_map(nodes_to_add, true);
+        }
+    }
+
+    template <class T, class Alloc>
+    void
+    deque<T, Alloc>::reallocate_map(size_type nodes_to_add, bool add_at_front){
+        size_type old_num_nodes = __finish().node - __start().node + 1;
+        size_type new_num_nodes = old_num_nodes + nodes_to_add;
+        map_pointer new_nstart;
+        if(__map_size > 2 * new_num_nodes){
+            new_nstart = __map + (__map_size - new_num_nodes) / 2 + (add_at_front ? nodes_to_add : 0);
+            if(new_nstart < __start().node){
+                std::copy(__start().node, __finish().node + 1, new_nstart);
+            }
+            else{
+                std::copy_backward(__start().node, __finish().node, new_nstart + old_num_nodes);
+            }
+        }
+        else{
+            size_type new_map_size = __map_size + std::max(__map_size, nodes_to_add) + 2;
+            map_pointer new_map = __map_allocator().allocate(new_map_size);
+            new_nstart = new_map + (new_map_size - new_num_nodes) / 2 + (add_at_front ? nodes_to_add : 0);
+            std::copy(__start().node, __finish().node + 1, new_nstart);
+            __map_allocator().deallocate(__map, __map_size);
+            __map = new_map;
+            __map_size = new_map_size;
+        }
+        __start().set_node(new_nstart);
+        __finish().set_node(new_nstart + old_num_nodes - 1);
+    }
+
+    template <class T, class Alloc>
+    template <class... Args>
+    typename deque<T, Alloc>::iterator
+    deque<T, Alloc>::insert_aux(iterator pos, Args&&... args){
+        const size_type elems_before = pos - __start();
+        value_type val_cp = value_type(std::forward<Args>(args)...);
+        if(elems_before < size() / 2){
+            emplace_front(front());
+            iterator front1 = __start();
+            ++front1;
+            iterator front2 = front1;
+            ++front2;
+            pos = __start() + elems_before;
+            iterator pos1 = pos;
+            ++pos1;
+            std::copy(front2, pos1, front1);
+        }
+        else{
+            emplace_back(back());
+            iterator back1 = __finish();
+            --back1;
+            iterator back2 = back1;
+            --back2;
+            pos = __start() + elems_before;
+            std::copy_backward(pos, back2, back1);
+        }
+        *pos = std::move(val_cp);
+        return pos;
     }
 
 } // namespace
