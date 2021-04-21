8037461104dba294f91c510ab1d2d03b01816956
IceCream-jy <304754843@qq.com>
2021-04-21 22:34:45 +0800

    add move/forward

--- STL/utility.h
+++ STL/utility.h
@@ -6,5 +6,28 @@
 ** 各种变态的元编程
 */
 
+#include <cstddef>
+#include "type_traits"
+
+namespace pocket_stl{
+    // move
+    template <class T>
+    typename std::remove_reference<T>::type move(T&& arg) noexcept{
+        return static_cast<typename std::remove_reference<T>::type&&>(arg);
+    }
+
+    template <class T>
+    T&& forward(typename std::remove_reference<T>::type& arg) noexcept{
+        return static_cast<T&&>(arg);
+    }
+
+    template <class T>
+    T&& forward(typename std::remove_reference<T>::type&& arg) noexcept{
+        static_assert(!std::is_lvalue_reference<T>::value, "bad forward");
+        return static_cast<T&&>(arg);
+    }
+
+}
+
 
 #endif
